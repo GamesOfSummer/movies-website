c331efc732eac65744b5e3f4f469fa3563a27fbc
Summer T <summer@summertime-development.com>
2024-09-05 17:54:10 -0500

    Button fix, cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { NavigationLink } from 'src/components';
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { setArray } from 'src/redux/moviesSlice';
 
 const Home: NextPage = () => {
-  //const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+  const [searchState, setSearchState] = useState('');
+
+  const handleChange = (event) => {
+    console.log(event);
+    const value = event.target.value;
+    setSearchState(value);
+  };
 
   async function getToken(): Promise {
     try {
@@ -17,14 +23,11 @@ const Home: NextPage = () => {
       );
 
       const token = response.data.token;
-
       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       const response2 = await axios.get(
         'https://0kadddxyh3.execute-api.us-east-1.amazonaws.com/movies'
       );
-
-      //console.log(response2.data.data);
 
       dispatch(setArray(response2.data.data));
     } catch (e: Error | AxiosError) {
@@ -40,31 +43,43 @@ const Home: NextPage = () => {
 
   getToken();
 
+  async function Search(): Promise {
+    try {
+      const response2 = await axios.get(
+        'https://0kadddxyh3.execute-api.us-east-1.amazonaws.com/movies?search=' +
+          searchState
+      );
+
+      console.log(response2.data.data);
+
+      // dispatch(setArray(response2.data.data));
+    } catch (e: Error | AxiosError) {
+      if (axios.isAxiosError(e)) {
+        console.log('AxiosError ------------------');
+        console.log(e);
+      } else {
+        console.log('Error ------------------');
+        console.log(e);
+      }
+    }
+  }
+
   return (
     <Fragment>
       <Head>
-        <title>next12-react-query-tailwind starter kit</title>
+        <title>Movies R Us</title>
         <meta name="description" content="Generated using starter.dev" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="w-3/5 my-5 mx-auto text-center">
         <h1 className="bg-blue-600 text-white text-2xl font-semibold p-4 rounded">
-          Create Next app with React Query and Tailwind
+          Movies R Us
         </h1>
-        <ul>
-          <li className="my-2.5">
-            <NavigationLink
-              to="/counter-example"
-              label="See Counter example component"
-            />
-          </li>
-          <li className="my-2.5">
-            <NavigationLink
-              to="/fetch-example"
-              label="See Fetch example component"
-            />
-          </li>
-        </ul>
+
+        <input type="text" placeholder="Search.." onChange={handleChange} />
+        <button type="button" onClick={() => Search()}>
+          Search!
+        </button>
       </header>
     </Fragment>
   );
